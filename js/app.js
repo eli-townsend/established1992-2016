7e54701733bf4ea4c7bc4fc89dc6fc8615ff6409
Elijah Townsend <elijahtownsend@Elijahs-MacBook-Pro-2.local>
2017-05-11 10:28:43 -0400

    styling/layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+function navToggle() {
+      var mainNav = document.querySelector('#main-navigation');
+      var navIcon = document.querySelector('#res-nav');
+      mainNav.classList.toggle('open');
+      navIcon.classList.toggle('open');
+    }
+      document.querySelector('#res-nav').addEventListener('click', navToggle);
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-43819097-1','established1992.com');ga('send','pageview');
